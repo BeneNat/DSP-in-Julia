3c8293f60f54e47feebc95399daa13c59b0abb89
BeneNat <165799435+BeneNat@users.noreply.github.com>
2024-04-18 16:02:05 +0200

    Add files via upload
    Lab05

---
+++ Lab05/lab05.jl
@@ -0,0 +1,52 @@
+# zadania przewidzane na dzisiaj:
+# 7.1.1 - 7.1.3; 7.2.1 - 7.2.7
+
+# problem 7.1.1
+##
+# problem tablicowy o podanych parametrach
+
+
+# problem 7.1.2
+##
+# problem tablicowy o podanych parametrach
+
+
+# problem 7.1.3
+##
+# problem tablicowy o podanych parametrach
+
+
+# problem 7.2.1
+##
+# funkcja fftfreq
+
+
+# problem 7.2.2
+##
+# funkcja rfftfreq
+
+
+# problem 7.2.3
+##
+# funkcja amplitude_spectrum
+
+
+# problem 7.2.4
+##
+# funkcja power_spectrum
+# warto zauwazyc ze to prawie dokladnie amplitude_spectrum
+
+
+# problem 7.2.5
+##
+# funkcja psd
+
+
+# problem 7.2.6
+##
+# funkcja periodogram
+
+
+# problem 7.2.7
+##
+# sygnal z radia
