3d01c25802fa408595bac75e3801fc364281be4f
BeneNat <165799435+BeneNat@users.noreply.github.com>
2024-04-25 16:27:48 +0200

    Lab06

---
+++ lab06/lab06.jl
@@ -0,0 +1,27 @@
+# zadania na dzisiaj
+# 7.2.8 - 7.2.11
+
+# problem 7.2.8
+##
+# stft
+
+# problem 7.2.9
+##
+# analiza czas-czestotliwosciowa i spektrogramy podanych sygnalow
+#= epic_sax_gux.wav,
+fail_trombone.wav,
+speech0001.wav. =#
+
+# problem 7.2.10
+##
+# istft
+
+#
+##
+# problem 7.2.11
+##
+# reprezentacja czasowo-czestotliwosciowa podanych sygnalow
+#= epic_sax_gux.wav,
+fail_trombone.wav,
+speech0001.wav. =#
+
