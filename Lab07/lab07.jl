942b38ceb88ac5528a18da098f8b5030baca082d
BeneNat <165799435+BeneNat@users.noreply.github.com>
2024-05-09 16:20:05 +0200

    Laboratorium 07

---
+++ Lab07/lab07.jl
@@ -0,0 +1,84 @@
+# zadania na dzisiaj:
+# 8.1 - 8.8
+# 8.1.1 - 8.1.8
+# systemy liniowe
+
+# problem 8.1
+##
+# czy system jest liniowy i niezmienny
+
+
+# problem 8.2
+##
+# wyznacz ciag wyj
+
+
+# problem 8.3
+##
+# czy system jest stabilny
+
+
+# problem 8.4
+##
+# odwrotna z transformacja
+
+
+# problem 8.5
+##
+# Fibonacci
+
+
+# problem 8.6
+##
+# wyznacz h2[n]
+
+
+# problem 8.7
+##
+# funkcja wlasna
+
+# problem 8.8
+##
+# rozwaz system
+
+# Problemy implementacyjne
+
+# problem 8.1.1
+##
+# funckja conv
+
+
+# problem 8.1.2
+##
+# funkcja fast_conv
+
+
+# problem 8.1.3
+##
+# funkcja overlap_add
+
+
+# problem 8.1.4
+##
+# funckja overlap_save
+
+
+# problem 8.1.5
+##
+# funckja lti_filter
+
+
+# problem 8.1.6
+##
+# funkcja filtfilt
+
+
+# problem 8.1.7
+##
+#funkcja lti_amp
+
+
+# problem 8.1.8
+##
+# funkcja lti_phase
+
